848edb3c9e9d5fbe4e26d40cce6a87b96e0d9631
ThietNN <nguyenthiet1225@gmail.com>
2022-05-06 15:14:33 +0700

    first commit

---
+++ src/main/java/thietnn/springboot_simpleweb/SpringBootSimpleWebApplication.java
@@ -0,0 +1,13 @@
+package thietnn.springboot_simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSimpleWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thietnn/springboot_simpleweb/SpringBootSimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package thietnn.springboot_simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
